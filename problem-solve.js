310d02d1e463e42a4dd75618e450b1df18129636
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-08 22:06:34 +0600

    argument

---
+++ problem-solve.js
@@ -0,0 +1,42 @@
+/*
+let p = 'javaScript';
+let q = p;
+p = 'React';
+console.log(q);
+
+const isTrue = 'false';
+if (!isTrue) {
+    console.log('hello');
+}
+else {
+    console.log('world');
+}
+
+function sum(p, q) {
+    p + q;
+}
+const result = sum(2, 3);
+console.log(result);
+
+if ("2" === 2) {
+    console.log('Inside if');
+}
+else {
+    console.log('Inside else')
+}
+
+function work(x, y = 4) {
+    return x + y;
+}
+console.log(work(32));
+
+*/
+
+/* 
+1. if i write []== true, will t show true or false?
+2. use === to check if the true === "true" give the true false.
+3. white an arrow function that wil take a parameter and will check the parameter is a number or not by using isNaN().and return the true false.
+4. 
+
+*/
+
